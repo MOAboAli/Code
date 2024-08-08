f9c7b237e843aaaf053f3aaea0912e6aaebca969
MoAboali <mohamed.aboali20@outlook.com>
2024-08-07 23:00:04 -0500

    Adding Home to front End

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './_main_components/footer/footer.component';
+import { HeaderComponent } from './_main_components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'FrontEnd';
--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ErrorPageComponent } from './_main_components/error-page/error-page.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", redirectTo: "home", pathMatch: "full"
+    },
+    {
+        path: "home", component: HomeComponent
+    },
+
+    // {
+    //     path: "games", component: GamesComponent
+    // },
+    // {
+    //     path: "games/game/:id", component: SingleGameComponent
+    // },
+    // {
+    //     path: "games/game/update/:id", component: UpdategameComponent
+    // },
+    // {
+    //     path: "games/Create", component: CreategameComponent
+    // },
+    {
+        path: "**",
+        component: ErrorPageComponent
+    }
+];
+
