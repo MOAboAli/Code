007b383afe557e654028ce848da3fcdfcfb9338a
MoAboali <mohamed.aboali20@outlook.com>
2024-08-10 04:52:35 -0500

    Adding Edit Feature

--- FrontEnd/src/app/components/cars/create-car/create-car.component.ts
+++ FrontEnd/src/app/components/cars/create-car/create-car.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { LoadingSymbolComponent } from '../../../_utility_components/loading-symbol/loading-symbol.component';
 import { CarsService } from '../_services/car.service';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormArray } from '@angular/forms';
 import { EditionDTO } from '../_dto/edition.model';
 import { CarsDTO } from '../_dto/car.model';
+import { ErrorHandlerService } from '../../../_service/error.service';
 
 @Component({
   selector: 'app-create-car',
@@ -15,20 +16,52 @@ import { CarsDTO } from '../_dto/car.model';
 })
 export class CreateCarComponent {
 
-
   loading: boolean = false;
   labeltest = 'Create a New Car';
   PageForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private router: Router, private Service: CarsService) {
+  _id: string = "";
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute, private Service: CarsService, private errorHandlerService: ErrorHandlerService) {
+    this._id = this.route.snapshot.params["id"];
+    this.loading = true;
     this.PageForm = this.fb.group({
       Make: ['', Validators.required],
       Model: [null, [Validators.required]],
       Year: [null, [Validators.min(1900), Validators.max(new Date().getFullYear())]],
-      imageUrl: [null, [Validators.min(0)]],
-
+      imageUrl: [null],
       Editions: this.fb.array(new Array<EditionDTO>)
     });
+
+    if (this._id && this._id != "") {
+      this.Service.getOneCars(this._id).subscribe(items => {
+
+        this.PageForm = this.fb.group({
+          Make: [items.ResponseData.Make, Validators.required],
+          Model: [items.ResponseData.Model, [Validators.required]],
+          Year: [items.ResponseData.Year, [Validators.min(1900), Validators.max(new Date().getFullYear())]],
+          imageUrl: [items.ResponseData.imageUrl],
+          Editions: this.fb.array([])//items.ResponseData.Editions!)
+        });
+
+        // Set the Editions FormArray with FormGroups
+        const editionsFormArray = this.PageForm.get('Editions') as FormArray;
+        items.ResponseData.Editions!.forEach((edition: EditionDTO) => {
+          editionsFormArray.push(this.fb.group({
+            Name: [edition.Name],
+            Features: [edition.Features],
+            Description: [edition.Description],
+            imageUrl: [edition.imageUrl]
+          }));
+        });
+
+        this.loading = false;
+      }, error => {
+        this.errorHandlerService.showError('An error occurred: ' + error.error.ResponseData);
+        this.loading = false;
+      });
+    }
+  }
+  ngOnInit(): void {
+
   }
 
   get subitems(): FormArray {
@@ -64,14 +97,20 @@ export class CreateCarComponent {
         formValues.imageUrl
       );
 
-      this.Service.CreateCars(item).subscribe(resp => {
-
-        console.log(resp);
-        if (!resp.hasOwnProperty('error')) {
-          this.router.navigate(['/cars']);
-        }
-      });
-
+      if (this._id && this._id != "") {
+        this.Service.UpdateCars(this._id, item).subscribe(resp => {
+          if (!resp.hasOwnProperty('error')) {
+            this.router.navigate(['/cars']);
+          }
+        });
+      }
+      else {
+        this.Service.CreateCars(item).subscribe(resp => {
+          if (!resp.hasOwnProperty('error')) {
+            this.router.navigate(['/cars']);
+          }
+        });
+      }
     }
   }
 }
